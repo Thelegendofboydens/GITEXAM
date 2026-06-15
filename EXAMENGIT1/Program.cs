c09b9d2fe02b9cb8feabae4889839d9aed2652b6
Thelegendofboydens <arnaudB803@lln.campussintursula.be>
2026-06-15 09:25:19 +0200

    BasisVersie

--- EXAMENGIT1/Program.cs
+++ EXAMENGIT1/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Basisklasse met eigenschap naam, leven en roll
+class Person
+{
+    public string Naam { get; set; }
+    public int Leven { get; set; }
+    public string Roll { get; set; }
+
+    public Person(string naam, int leven, string roll)
+    {
+        Naam = naam;
+        Leven = leven;
+        Roll = roll;
+    }
+
+    public virtual void ToonInfo()
+    {
+        Console.WriteLine($"Naam: {Naam}");
+        Console.WriteLine($"Leven: {Leven}");
+        Console.WriteLine($"Roll: {Roll}");
+    }
+}
+
+// Afgeleide klasse die inheritance gebruikt
+class Personage : Person
+{
+    public Personage(string naam, int leven, string roll)
+        : base(naam, leven, roll)
+    {
+    }
+
+    public override void ToonInfo()
+    {
+        Console.WriteLine("Personagegegevens:");
+        base.ToonInfo();
+    }
+}
+
+// Maak een object van de klasse Personage en toon de gegevens
+class Program
+{
+    static void Main()
+    {
+        var mijnPersonage = new Personage("Arend", 100, "Strijder");
+        mijnPersonage.ToonInfo();
+    }
+}
